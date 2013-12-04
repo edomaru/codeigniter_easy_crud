5f784f948ad230cf6073d90b7a8598c19002ac10
edomaru <masaruedo@yahoo.com>
2013-12-04 09:12:59 +0700

    configure aplication

--- application/config/config.php
+++ application/config/config.php
@@ -26,7 +26,7 @@ $config['base_url']	= '';
 | variable so that it is blank.
 |
 */
-$config['index_page'] = 'index.php';
+$config['index_page'] = '';
 
 /*
 |--------------------------------------------------------------------------
@@ -224,7 +224,7 @@ $config['cache_path'] = '';
 | MUST set an encryption key.  See the user guide for info.
 |
 */
-$config['encryption_key'] = '';
+$config['encryption_key'] = 'ec68bd5f61665664246c9b92bb38dfda15c285b0';
 
 /*
 |--------------------------------------------------------------------------
